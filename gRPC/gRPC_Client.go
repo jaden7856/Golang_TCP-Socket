597f6dc09dc4fb879cfa5f5f69159e313175662b
Jaden Jo <jhg7856@gmail.com>
2021-12-21 17:10:40 +0900

    gRPC를 이요한 Client-side

--- gRPC/gRPC_Client.go
+++ gRPC/gRPC_Client.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"google.golang.org/grpc"
 	"log"
+	"runtime"
 	"time"
+
+	pb "github.com/jaden7856/Golang_TCP-Socket/gRPC/protoc"
+	"google.golang.org/grpc"
 )
 
+var (
+	msg1 = "client-1 입니다."
+	msg2 = "client-2 입니다."
+	msg3 = "client-3 입니다."
+)
+
+func callSendMsg(c pb.GRPCSendMsgClient, msg string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	r, err := c.SendMsg(ctx, &pb.MessageRequest{Message: msg})
+	if err != nil {
+		log.Fatalf("could not send : %v", err)
+	}
+	log.Printf("Sending : %s", r.GetMessage())
+}
+
 func main() {
-	conn, err := grpc.Dial(":8080")
+	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	defer conn.Close()
 
-	c := pb.NewGrpcSendMsgClient(conn)
+	msgc := pb.NewGRPCSendMsgClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	go callSendMsg(msgc, msg1)
+	runtime.Gosched()
+	go callSendMsg(msgc, msg2)
+	runtime.Gosched()
+	callSendMsg(msgc, msg3)
 
-	var s string
-	fmt.Print("입력하세요 : ")
-	fmt.Scanln(&s)
+	//eg, _ := errgroup.WithContext(context.Background())
+	//for i := 0; i < 3; i++ {
+	//	eg.Go(func() error {
+	//		callSendMsg(msgc, msg)
+	//		return nil
+	//	})
+	//}
+	//if err := eg.Wait(); err != nil {
+	//	panic(err)
+	//}
 
-	r, err := c.SendMsg(ctx, &pb.MessageRequest{Message: s})
-	if err != nil {
-		log.Fatalf("could not send : %v", err)
-	}
-	log.Printf("Sending : %s", r.GetMessage())
 }
